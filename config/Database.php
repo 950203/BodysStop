b6efa24241f53104e300ba73d0f9af5980c1c8b7
Ingrid Riveros <juliethingrid1023@gmail.com>
2026-07-30 23:23:24 +0000

    feat: actualizar configuración de conexión a la base de datos y mejorar el archivo docker-compose

--- config/Database.php
+++ config/Database.php
@@ -7,10 +7,10 @@ class Database
     public static function getConexion(): PDO
     {
         if (self::$db === null) {
-            $host = getenv('DB_HOST') ?: 'localhost';
+            $host = getenv('DB_HOST') ?: 'db';
             $dbname = getenv('DB_NAME') ?: 'bodyshop';
             $user = getenv('DB_USER') ?: 'root';
-            $pass = getenv('DB_PASS') ?: '';
+            $pass = getenv('DB_PASS') ?: 'root';
             self::$db = new PDO(
                 "mysql:host=$host;dbname=$dbname;charset=utf8",
                 $user,
